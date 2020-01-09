e745bd1b4325f4f3e54e542971e5d6ee896077ec
Nathan Nguyen <nathannguyen@catalyst-au.net>
2020-01-10 10:38:53 +1100

    Change event crud

--- classes/event/sitenotice_deleted.php
+++ classes/event/sitenotice_deleted.php
@@ -30,7 +30,7 @@ class sitenotice_deleted extends \core\event\base {
 
     protected function init() {
         $this->data['objecttable'] = 'local_sitenotice';
-        $this->data['crud'] = 'u';
+        $this->data['crud'] = 'd';
         $this->data['edulevel'] = self::LEVEL_OTHER;
     }
 
